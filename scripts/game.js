6bfa193b0be2eb8aaf7c51c7e9c7d490bf99c3d9
Богдан <brazbogdan@gmail.com>
2025-02-21 15:24:43 +0200

    updated commit

--- scripts/game.js
+++ scripts/game.js
@@ -25,7 +25,7 @@ app.stage.addChild(background);
 isGamePaused : true,
 // Параметри астероїдів
 asteroidData : { spawnedAsteroids: 0 },
- totalAsteroids : 1,
+ totalAsteroids : 10,
  asteroids: [],
  bulletData : { shotsFired : 0 },
   maxBullets : 10, // Встановлюємо максимальну кількість куль
