13633ef7944f8179fb071b01f43810300e7eb6ea
Богдан <brazbogdan@gmail.com>
2025-02-23 19:30:24 +0200

    updated commit

--- scripts/objects/spaceshipBullets.js
+++ scripts/objects/spaceshipBullets.js
@@ -1,22 +1,9 @@
 import {app, gameState} from '../game.js';
+import { moveSpaceshipBullets } from '../tickerCallbacks/moveSpaceshipBullets.js';
 
 
 export const spaceshipBullets = () => {
     
-   
-    const moveSpaceshipBullets = () => {
-       
-
-        for (let i = gameState.bullets.length - 1; i >= 0; i--) {
-            if(gameState.bullets[i]){
-           gameState.bullets[i].y -= 3;
-            if (gameState.bullets[i].y < 0) {
-                app.stage.removeChild(gameState.bullets[i]);
-                gameState.bullets.splice(i, 1);
-            }
-        }
-        }
-    };
     app.ticker.add(moveSpaceshipBullets);
     gameState.tickerCallbacks.push(() => app.ticker.remove(moveSpaceshipBullets));
     
