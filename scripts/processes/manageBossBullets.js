652145b055e3762f43cc4a437531753b28d75225
Богдан <brazbogdan@gmail.com>
2025-02-24 10:54:45 +0200

    updated commit

--- scripts/processes/manageBossBullets.js
+++ scripts/processes/manageBossBullets.js
@@ -13,69 +13,7 @@ export const manageBossBullets = (app, spaceship, boss, bossBullets) => {
     app.ticker.add(bossBulletMovementCallback);
     gameState.tickerCallbacks.push(() => app.ticker.remove(bossBulletMovementCallback));
 
-    const bossProcesses = () => {
-        for (let i = bossBullets.length - 1; i >= 0; i--) {
-            if (hitTestRectangle(bossBullets[i], gameState.spaceship)) {
-                app.stage.removeChild(bossBullets[i]);
-                endGame(app, "YOU LOSE", "red");
-                clearInterval(spawnIntervalBossBullets); // Додано цей рядок для очищення інтервалу
-                return;
-            }
-        }
-
-        // Рух і перевірка колізій куль корабля
-        for (let j = gameState.bullets.length - 1; j >= 0; j--) {
-            if (!gameState.bullets[j]) continue;
-
-            if (gameState.bullets[j].y < 0) {
-                app.stage.removeChild(gameState.bullets[j]);
-                gameState.bullets.splice(j, 1);
-                if (gameState.bulletData.shotsFired >= gameState.maxBullets && gameState.bossHP > 0) {
-                    endGame(app, "YOU LOSE", "red");
-                    clearInterval(spawnIntervalBossBullets); // Додано цей рядок для очищення інтервалу
-                    return;
-                }
-                continue;
-            }
-
-            if (typeof gameState.bullets[j].collided === "undefined") {
-                gameState.bullets[j].collided = false;
-            }
-            if (gameState.bullets[j].collided) continue;
-
-            let collidedWithBossBullet = false;
-
-            // Перевірка зіткнення кулі корабля з кулею боса
-            for (let i = bossBullets.length - 1; i >= 0; i--) {
-                if (hitTestRectangle(gameState.bullets[j], bossBullets[i])) {
-                    app.stage.removeChild(gameState.bullets[j]);
-                    app.stage.removeChild(bossBullets[i]);
-                    gameState.bullets[j].collided = true;
-                    gameState.bullets.splice(j, 1);
-                    bossBullets.splice(i, 1);
-                    collidedWithBossBullet = true;
-                    break;
-                }
-            }
-
-            if (collidedWithBossBullet) continue;
-
-            // Перевірка зіткнення кулі корабля з босом
-            if (hitTestRectangle(gameState.bullets[j], boss)) {
-                gameState.bullets[j].collided = true;
-                app.stage.removeChild(gameState.bullets[j]);
-                gameState.bullets.splice(j, 1);
-                gameState.bossHP--;
-                bossHPBar.text = `Boss HP: ${gameState.bossHP}`;
-                if (gameState.bossHP === 0) {
-                    clearInterval(spawnIntervalBossBullets); // Додано цей рядок для очищення інтервалу
-                    endGame(app, "YOU WIN", "green", true);
-                    return;
-                }
-                continue;
-            }
-        }
-    };
+    
 
     app.ticker.add(bossProcesses);
     gameState.tickerCallbacks.push(() => app.ticker.remove(bossProcesses));
