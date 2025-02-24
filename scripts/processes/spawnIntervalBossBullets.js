889044d8ff53f05fe12395caad12d3b0767c0287
Богдан <brazbogdan@gmail.com>
2025-02-24 11:01:35 +0200

    updated commit

--- scripts/processes/spawnIntervalBossBullets.js
+++ scripts/processes/spawnIntervalBossBullets.js
@@ -1,3 +1,5 @@
+  import {spawnbossBullet} from '../objects/spawnbossBullet.js';
+  
   export const spawnIntervalBossBullets = setInterval(() => {
         const bullet = spawnbossBullet(app, boss);
         bossBullets.push(bullet);
