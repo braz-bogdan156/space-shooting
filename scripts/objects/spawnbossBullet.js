5afff9142dcb781424cb3b21d5aba2f2b3a8f4f8
Богдан <brazbogdan@gmail.com>
2025-02-24 13:20:29 +0200

    updated commit

--- scripts/objects/spawnbossBullet.js
+++ scripts/objects/spawnbossBullet.js
@@ -1,20 +1,15 @@
 import { gameState, app } from "../game.js";
-import { createBoss } from "./boss.js";
+
 // Функція для створення однієї пулі боса
 export const spawnbossBullet = () => {
-  gameState.boss = createBoss(app);
-  
  
-
-    const bossBullet = new PIXI.Graphics();
+  const bossBullet = new PIXI.Graphics();
     bossBullet.beginFill(0xff0000);
     bossBullet.drawCircle(0, 0, 10);
     bossBullet.endFill();
     bossBullet.x = gameState.boss.x + gameState.boss.width / 2;
     bossBullet.y = gameState.boss.y + gameState.boss.height;
     app.stage.addChild(bossBullet);
-
-    
 
     return bossBullet;
 };
