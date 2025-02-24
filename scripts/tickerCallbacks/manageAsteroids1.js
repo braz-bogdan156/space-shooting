f9ae88ffa8f0bbdeeeb1407d98c70c9720081750
Богдан <brazbogdan@gmail.com>
2025-02-24 08:12:39 +0200

    updated commit

--- scripts/tickerCallbacks/manageAsteroids1.js
+++ scripts/tickerCallbacks/manageAsteroids1.js
@@ -34,9 +34,10 @@ export const manageAsteroids1 = () => {
                 app.stage.removeChild(gameState.asteroids[i]);
                 gameState.asteroids.splice(i, 1);
                 }
+                app.stage.removeChild(gameState.bullets[j]);
+                gameState.bullets.splice(j, 1);
         }
-        app.stage.removeChild(gameState.bullets[j]);
-        gameState.bullets.splice(j, 1);
+       
         
         // Перевірка колізії астероїда з кораблем
         if (gameState.asteroids[i] && hitTestRectangle(gameState.asteroids[i], gameState.spaceship)) {
