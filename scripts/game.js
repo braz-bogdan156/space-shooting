b925a61962de075eb5e110332d0f8499e7bf774b
Богдан <brazbogdan@gmail.com>
2025-02-27 16:57:37 +0200

    updated commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,58 +1,57 @@
-import { createStartButton } from './objects/startButton.js';
-import {startGame} from './processes/startGame.js';
+import { createStartButton } from "./objects/startButton.js";
+import { startGame } from "./processes/startGame.js";
 
 export const app = new PIXI.Application({
-    width: 1280,
-    height: 720,
+  width: 1280,
+  height: 720,
 });
 
 document.body.appendChild(app.view);
 
-// === ІНІЦІАЛІЗАЦІЯ ГРИ (Об'єкти створені, але гра на паузі) ===
 
 // Фон
-const backgroundTexture = PIXI.Texture.from('assets/images/stairs.png');
- export const background = new PIXI.Sprite(backgroundTexture);
+const backgroundTexture = PIXI.Texture.from("assets/images/stairs.png");
+export const background = new PIXI.Sprite(backgroundTexture);
 background.width = app.screen.width;
 background.height = app.screen.height;
 app.stage.addChild(background);
 
- export const  gameState = {
- spaceship : null,
- bullets : [],
-isGamePaused : true,
-// Параметри астероїдів
-asteroidData : { spawnedAsteroids: 0 },
- totalAsteroids : 10,
- asteroids: [],
- currentSpeed: 0.8, 
- destroyedAsteroids : new Set(),
- destroyedBullets: new Set(),
- spawnAsteroidsInterval: null,
-   bulletData : { shotsFired : 0 },
-  maxBullets : 10, // Встановлюємо максимальну кількість куль
-   keys : {},
- shootingState : {
-    shooting : false
-},
- canShoot : true,
- spaceshipSpeed : 10,
- spaceshipAcceleration : 0.2, // Початкове прискорення космічного корабля
- maxSpaceshipSpeed : 18, // Максимальна швидкість космічного корабля
- bulletCounterText : null ,
-    boss: null,
-    bossBullets : [],
-     bossHP : 4,
-     bossHPBar : null,
-     spawnBossInterval : null,
-     collidedWithBossBullet: false,
-   tickerCallbacks: [],
-   timerInterval: null,
-   bossShouldMove: true, 
-   randomBossBullets : [],
-   alternate: true,
- }
+export const gameState = {
+  spaceship: null,
+  bullets: [],
+  isGamePaused: true,
+  // Параметри астероїдів
+  asteroidData: { spawnedAsteroids: 0 },
+  totalAsteroids: 10,
+  asteroids: [],
+  currentSpeed: 0.8,
+  destroyedAsteroids: new Set(),
+  destroyedBullets: new Set(),
+  spawnAsteroidsInterval: null,
+  bulletData: { shotsFired: 0 },
+  maxBullets: 10, // Встановлюємо максимальну кількість куль
+  keys: {},
+  shootingState: {
+    shooting: false,
+  },
+  canShoot: true,
+  spaceshipSpeed: 10,
+  spaceshipAcceleration: 0.2, // Початкове прискорення космічного корабля
+  maxSpaceshipSpeed: 18, // Максимальна швидкість космічного корабля
+  bulletCounterText: null,
+  boss: null,
+  bossBullets: [],
+  bossHP: 4,
+  bossHPBar: null,
+  spawnBossInterval: null,
+  collidedWithBossBullet: false,
+  tickerCallbacks: [],
+  timerInterval: null,
+  bossShouldMove: true,
+  randomBossBullets: [],
+  alternate: true,
+};
 
 // Кнопка старту гри
- const startButton = createStartButton(app, startGame);
+const startButton = createStartButton(app, startGame);
 app.stage.addChild(startButton);
